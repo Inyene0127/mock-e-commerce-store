c96381fffd74aae6a0acf45993ce668fb3b4f84c
Inyene Eduok <eduok.inyene@stu.cu.edu.ng>
2024-03-08 15:17:18 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatTableModule } from '@angular/material/table'
+import { MatBadgeModule } from '@angular/material/badge'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatListModule } from '@angular/material/list'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule,
+    MatListModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
